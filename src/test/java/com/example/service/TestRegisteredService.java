4c5fc19e7b24630bb64bc34af8e141aedac013d1
yangwang <1261368581@qq.com>
2017-04-17 21:21:58 +0800

    编写注册service的测试用例

--- src/test/java/com/example/service/TestRegisteredService.java
+++ src/test/java/com/example/service/TestRegisteredService.java
@@ -1,8 +1,15 @@
 package com.example.service;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@SpringBootTest
 public class TestRegisteredService {
 	
 	@Autowired
